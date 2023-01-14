6550ebcaf184e13399cde9be93a9c504ef9fcb26
isakashiori <isslamdunk0000@gmail.com>
2023-01-14 15:02:43 +0900

    add: listコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import About from "./components/About";
 import Login from "./components/Login";
 import Authentication from "./components/Authentication";
 import Header from './components/Header';
+import List from './components/List';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
           <Route path="/login" element={<Authentication />} />
+          <Route path="/list" element={<List />} />
         </Routes>
       </div>
     </Router>
